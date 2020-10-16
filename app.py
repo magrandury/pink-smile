2dcd0bf6b676c1f406b8ed946f7aacb8b0004206
Maria Grandury <mariagrandury@gmail.com>
2020-10-16 15:37:00 +0200

    Add PinkBot and Organizations sections

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ def stats():
     return render_template("stats.html")
 
 
+@app.route("/pinkbot")
+def pinkbot():
+    return render_template("pinkbot.html")
+
+
+@app.route("/organizations")
+def organizations():
+    return render_template("organizations.html")
+
+
 @app.route("/stories")
 def stories():
     return render_template("stories.html")
@@ -20,6 +30,7 @@ def stories():
 @app.route("/media")
 def media():
     return render_template("media.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
